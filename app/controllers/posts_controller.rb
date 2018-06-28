1fbccdf87af67626899e812505014bbfa349523e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-28 14:58:18 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params(:title))
+    @post.update(post_params(:title, :description, :post_status))
     redirect_to post_path(@post)
   end
 
